cecb2753f0902b9c21ae98fe34615bb002d65d11
code2811 <f.kayiranga@alustudent.com>
2024-10-04 07:14:43 -0700

    create_environment.sh

---
+++ create_environment.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+mkdir submission_reminder_app
+
+cd submission_reminder_app
+mkdir src
+mkdir data
+mkdir tests
+touch README.md
+touch src/main.py
+touch tests/test_main.py
+echo "Environment setup complete.
+
